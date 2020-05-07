cf4a53c89b4bddbe8395093bcdb819d74c31005c
Kevin Gomes <kevin28bruno@gmail.com>
2020-05-07 10:15:58 -0400

    Criacao do projeto

---
+++ src/main/java/br/com/kbmg/wspontoonline/WsPontoOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.kbmg.wspontoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPontoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPontoOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kbmg/wspontoonline/WsPontoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kbmg.wspontoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsPontoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
